026c7df15184be2ae2890e01dbe46540cd656ee7
Burakhan Alagöz <burakhanalagoz@gmail.com>
2022-02-14 15:01:26 +0300

    v1.1

--- main.js
+++ main.js
@@ -28,13 +28,33 @@ client.on('messageCreate', async msg =>
     //makes a guess in a wordle game
     if(msg.content.includes("!g"))
     {
-        Wordle.PlayWordle(msg);
+        //search word in list
+        var guess = msg.content.split(" ")[1];
+        guess = guess.toUpperCase();
+        var sonuc = answers.indexOf(guess);
+        if(sonuc >= 0)
+        {
+            Wordle.PlayWordle(msg);
+        }
+        else
+        {
+            msg.reply("This word is not available in the list.");
+        }
+        
     }
     //shows your stats
     if(msg.content.includes("!ws"))
     {
         Wordle.ShowWordleStats(msg);
     }
+
+    if(msg.content.includes("!player"))
+    {
+        msg.reply(msg.author.username);
+    }
 });
 
-client.login("OTQyNDY2Njk4MjExMTE1MTI5.Ygk6gw.DyLqz-UNG7Zy4pXWwZigiO4leSc");
+//client.login("OTQyNDY2Njk4MjExMTE1MTI5.Ygk6gw.Ua2oBakYWjk1HY-aHF6GUGuSsEQ");
+const dotenv = require('dotenv');
+dotenv.config();
+client.login(process.env.API_KEY);
